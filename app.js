76d952f8db207e6243094d0f01cab623100f6681
vitaly.smirnov <Vitaly.Smirnov@nexign-systems.com>
2020-08-09 03:38:35 +0300

    add bodyParser

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require("express");
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const app = express();
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+// set port, listen for requests
+app.listen(3000, () => {
+  console.log("Server is running on port 3000.");
 });
