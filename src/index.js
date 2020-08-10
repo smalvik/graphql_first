52cef86232fa19f8b7429ec636771f882628db6d
vitaly.smirnov <Vitaly.Smirnov@nexign-systems.com>
2020-08-10 21:56:59 +0300

    start project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log(2);
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const schema = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello client!'
+    }
+}
+
+const PORT = 4000;
+
+const app = express();
+
+const server = new ApolloServer({
+    typeDefs: schema, 
+    resolvers
+});
+
+server.applyMiddleware({app});
+
+app.listen({ port: PORT }, () => 
+    console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`)
+);
